9f4b4df1d7bb5f23cd540cdb744c4d349a096500
ezequiel <ezefiigueroa@gmail.com>
2025-08-22 20:57:01 -0300

    Agregué campos del modelo persona y trabajé en las vistas y urls de la app persona.

--- crud/persona/models.py
+++ crud/persona/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    """Model definition for Persona."""
+
+    nombre = models.CharField(verbose_name="Nombre", max_length=50)
+    apellido = models.CharField(verbose_name="Apellido", max_length=50)
+    edad = models.IntegerField(verbose_name="Edad")
+
+    class Meta:
+        """Meta definition for Persona."""
+
+        verbose_name = 'Persona'
+        verbose_name_plural = 'Personas'
+
+    def __str__(self):
+        """Unicode representation of Persona."""
+        return f"Nombre: {self.nombre} {self.apellido}, Edad: {self.edad}"
--- crud/persona/views.py
+++ crud/persona/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Persona
 
-# Create your views here.
+
+class PersonaListView(ListView):
+    model = Persona
+    template_name = "persona/lista.html"
+    context_object_name = "personas"
