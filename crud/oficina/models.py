b7b63b08273af3089354a568d2bb866fc91fb2ad
facufigueroa <facundoleonelfigueroa@hotmail.com>
2025-08-24 16:20:14 -0300

    Modifiqué campos del modelo oficina.

--- crud/oficina/models.py
+++ crud/oficina/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Oficina(models.Model):
     """Model definition for Oficina."""
 
-    nombre = models.CharField(verbose_name="Nombre", max_length=50)
-    nombreCorto = models.CharField(verbose_name="Nombre corto", max_length=50)
+    nombre = models.CharField(verbose_name="Nombre", max_length=50, unique=True)
+    nombreCorto = models.SlugField(verbose_name="Nombre corto", max_length=10, unique=True, help_text="Siglas de la oficina")
 
     class Meta:
         """Meta definition for Oficina."""
