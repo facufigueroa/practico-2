ade71c9092d5fe5397608aa7a0feafa250ea38e9
facufigueroa <facundoleonelfigueroa@hotmail.com>
2025-08-24 21:27:11 -0300

    Agregué vistas principales de oficina y persona.

--- crud/oficina/views.py
+++ crud/oficina/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Oficina
 
 
@@ -7,3 +8,26 @@ class OficinaListView(ListView):
     model = Oficina
     template_name = "oficina/lista.html"
     context_object_name = "oficinas"
+
+class OficinaDetailView(DetailView):
+    model = Oficina
+    template_name = "oficina/detalle.html"
+    context_object_name = "oficina"
+
+class OficinaCreateView(CreateView):
+    model = Oficina
+    template_name = "oficina/crear.html"
+    fields = ['nombre', 'nombreCorto']
+    success_url = reverse_lazy('oficina:lista')
+
+class OficinaUpdateView(UpdateView):
+    model = Oficina
+    template_name = "oficina/crear.html"
+    fields = ['nombre', 'nombreCorto']
+    success_url = reverse_lazy('oficina:lista')
+
+class OficinaDeleteView(DeleteView):
+    model = Oficina
+    template_name = "oficina/eliminar.html"
+    success_url = reverse_lazy('oficina:lista')
+    context_object_name = "oficina"
--- crud/persona/views.py
+++ crud/persona/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Persona
-
 
 class PersonaListView(ListView):
     model = Persona
     template_name = "persona/lista.html"
     context_object_name = "personas"
+
+class PersonaDetailView(DetailView):
+    model = Persona
+    template_name = "persona/detalle.html"
+    context_object_name = "persona"
+
+class PersonaCreateView(CreateView):
+    model = Persona
+    template_name = "persona/crear.html"
+    fields = ['nombre', 'apellido', 'edad', 'oficina']
+    success_url = reverse_lazy('persona:lista')
+
+class PersonaUpdateView(UpdateView):
+    model = Persona
+    template_name = "persona/crear.html"
+    fields = ['nombre', 'apellido', 'edad', 'oficina']
+    success_url = reverse_lazy('persona:lista')
+
+class PersonaDeleteView(DeleteView):
+    model = Persona
+    template_name = "persona/eliminar.html"
+    success_url = reverse_lazy('persona:lista')
+    context_object_name = "persona"
